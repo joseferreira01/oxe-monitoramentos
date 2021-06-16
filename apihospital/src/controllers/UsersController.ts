75729a573e11de58b6084c5be57aed93cf8cbf4d
Klihsman <klihsman2.0@gmail.com>
2021-06-16 12:10:47 -0300

    Criando entidades

--- apihospital/src/controllers/UsersController.ts
+++ apihospital/src/controllers/UsersController.ts
@@ -2,6 +2,7 @@ import { User } from '@models/User'
 
 export class UsersController {
   teste () {
+    // eslint-disable-next-line no-unused-vars
     const user = new User()
   }
 }
---
+++ apihospital/src/models/Institution.ts
@@ -0,0 +1,8 @@
+export class Institution {
+    name: string;
+    id: string;
+    type: string;
+    utiBeds: number;
+    infBeds: number;
+    responsible: string;
+}
